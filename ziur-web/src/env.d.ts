9268051a9ea201ea095600c2fe2c6474678a7043
jongoikolea <jon.goikolea@gmail.com>
2024-10-18 18:52:54 +0200

    added QR

--- ziur-web/src/env.d.ts
+++ ziur-web/src/env.d.ts
@@ -1,1 +1,6 @@
 /// <reference path="../.astro/types.d.ts" />
+declare function qrcode(type: number, errorCorrectionLevel: string): {
+    addData(data: string): void;
+    make(): void;
+    createSvgTag(cellSize: number, margin: number): string;
+};
